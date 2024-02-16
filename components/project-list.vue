a4106a43c6048248b174ab817ae7f317c4cac429
hoang227 <hoangg227@gmail.com>
2024-02-16 18:29:59 +0800

    display repositories on projects page

--- components/project-list.vue
+++ components/project-list.vue
@@ -1,13 +1,31 @@
 
-import type { errorMessages } from 'vue/compiler-sfc';
 <template>
-  <div>
-    projects
-  </div>
+  <p class="mb-10">take a look at my github projects</p>
 
   <section v-if="pending">loading ...</section>
   <section v-else-if="error">something went wrong ...</section>
-  <section v-else>here we display the repos</section>
+  <section v-else>
+    <ul class="grid grid-cols-1 gap-4">
+      <li
+        v-for="repo in data"
+        :key="repo.id"
+        class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+      >
+        <a
+          :href="repo.html_url"
+          target="_blank"
+        >
+          <div class="flex items-center justify-between">
+            <div class="font-semibold">{{ repo.name }}</div>
+            <div>
+              {{ repo.created_at }}
+            </div>
+          </div>
+        </a>
+
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
