80b2cbdb90bfa4dbbc5fc9c494499517041c840f
hoang227 <hoangg227@gmail.com>
2024-02-16 18:14:45 +0800

    set up data fetching to fetch repos data from github

--- components/project-list.vue
+++ components/project-list.vue
@@ -1,5 +1,15 @@
+
+import type { errorMessages } from 'vue/compiler-sfc';
 <template>
   <div>
     projects
   </div>
+
+  <section v-if="pending">loading ...</section>
+  <section v-else-if="error">something went wrong ...</section>
+  <section v-else>here we display the repos</section>
 </template>
+
+<script setup>
+const { error, pending, data } = await useFetch('https://api.github.com/users/hoang227/repos')
+</script>
