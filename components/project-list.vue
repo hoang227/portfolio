961789a555380f54a92233f82a1ab959b2d0cc8b
hoang227 <hoangg227@gmail.com>
2024-02-19 17:10:33 +0800

    filter out repos with description then sort them

--- components/project-list.vue
+++ components/project-list.vue
@@ -7,7 +7,7 @@
   <section v-else>
     <ul class="grid grid-cols-1 gap-4">
       <li
-        v-for="repo in data"
+        v-for="repo in repos"
         :key="repo.id"
         class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
       >
@@ -18,9 +18,12 @@
           <div class="flex items-center justify-between">
             <div class="font-semibold">{{ repo.name }}</div>
             <div>
-              {{ repo.created_at }}
+              {{ repo.stargazers_count }} ★
             </div>
           </div>
+          <p class="text.sm">
+            {{ repo.description }}
+          </p>
         </a>
 
       </li>
@@ -29,5 +32,21 @@
 </template>
 
 <script setup>
+/* 
+imports
+*/
+import { computed } from 'vue'
+
+/* 
+fetch repos
+*/
 const { error, pending, data } = await useFetch('https://api.github.com/users/hoang227/repos')
+
+/*
+sort repos
+*/
+const repos = computed( // filter out repos with description then sort them
+  () => data.value.filter(repo => repo.description)
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+)
 </script>
